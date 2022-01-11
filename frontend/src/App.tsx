ba4c4bce295114fb64f05be86d499eb1a2457f42
Flávio Santana <flavio_3310@hotmail.com>
2022-01-11 00:01:39 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import Navbar from "./components/NavBar";
+
 function App() {
   return (
-   <h1>Página principal</h1>
+   <Navbar />
   );
 }
 
---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import './styles.css';
+function Navbar() {
+  return (
+    <header>
+      <nav className="conteiner">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/flavio3310/dsmovie">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/dvsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
